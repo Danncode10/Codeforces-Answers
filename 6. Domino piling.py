d1b704f7479637441054b0cf48699a3ad9c20df8
Dann <Lesterdannlopez7@gmail.com>
2025-02-28 13:56:58 +0800

    Solved: 6. Domino piling

---
+++ 6. Domino piling.py
@@ -0,0 +1,9 @@
+# link: https://codeforces.com/problemset/problem/50/A
+# 800 elo
+# Time finished: 05:28
+
+import math
+m, n = map(int, input().split())
+
+result = math.floor((m * n)/2)
+print(result)
