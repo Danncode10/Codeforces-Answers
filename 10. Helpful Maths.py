a2d33a609de3a9815c548aa077a9e976267f5511
Dann <Lesterdannlopez7@gmail.com>
2025-03-01 21:49:09 +0800

    Solved: 10. Helpful Maths

---
+++ 10. Helpful Maths.py
@@ -0,0 +1,18 @@
+# Link: https://codeforces.com/problemset/problem/339/A
+# 800 elo
+
+inp = list(map(int, input().split('+')))
+
+inp.sort()
+
+n = len(inp)
+output = ""
+for i in range(n-1):
+    output = output + str(inp[i]) + "+"
+
+output = output + str(inp[-1])
+
+print(output)
+
+
+
